bd397e2d694f76fad5659bf4855b3a5ac3dfb3b2
Shirisha Kesagoni <kesagonishirisha@gmail.com>
2024-07-28 14:24:10 -0500

    Commiting Assignment5

---
+++ customer/src/main/java/com/amigoscode/customer/CustomerService.java
@@ -0,0 +1,15 @@
+package com.amigoscode.customer;
+
+public record CustomerService(CustomerRepository customerRepository) {
+
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+        // todo: check if email valid
+        // todo: check if email not taken
+        customerRepository.save(customer);
+    }
+}
